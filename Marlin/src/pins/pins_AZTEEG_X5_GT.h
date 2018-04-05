07b4c6a8f4c1b58663a3ecb3fdbc63ed1aee7085
Alexey Shvetsov <alexxy@gentoo.org>
2018-04-06 00:31:28 +0300

    Make code actualy compile with LPC for Azteeg

--- Marlin/src/pins/pins_AZTEEG_X5_GT.h
+++ Marlin/src/pins/pins_AZTEEG_X5_GT.h
@@ -53,6 +53,8 @@
 #define X_DIR_PIN          P0_11
 #if DISABLED(X_IS_TMC2660)
   #define X_ENABLE_PIN     P0_10
+#else
+  #define X_ENABLE_PIN     -1
 #endif
 #ifndef X_CS_PIN
   #define X_CS_PIN         P0_10 // BSD2660 default
@@ -62,6 +64,8 @@
 #define Y_DIR_PIN          P0_20
 #if DISABLED(Y_IS_TMC2660)
   #define Y_ENABLE_PIN     P0_19
+#else
+  #define Y_ENABLE_PIN     -1
 #endif
 #ifndef Y_CS_PIN
   #define Y_CS_PIN         P0_19 // BSD2660 default
@@ -71,6 +75,8 @@
 #define Z_DIR_PIN          P0_22
 #if DISABLED(Z_IS_TMC2660)
   #define Z_ENABLE_PIN     P0_21
+#else
+  #define Z_ENABLE_PIN     -1
 #endif
 #ifndef Z_CS_PIN
   #define Z_CS_PIN         P0_21 // BSD2660 default
@@ -80,6 +86,8 @@
 #define E0_DIR_PIN         P0_05
 #if DISABLED(E0_IS_TMC2660)
   #define E0_ENABLE_PIN    P0_04
+#else
+  #define E0_ENABLE_PIN    -1
 #endif
 #ifndef E0_CS_PIN
   #define E0_CS_PIN        P0_04 // BSD2660 default
@@ -89,6 +97,8 @@
 #define E1_DIR_PIN         P2_13
 #if DISABLED(E1_IS_TMC2660)
   #define E1_ENABLE_PIN    P4_29
+#else
+  #define E1_ENABLE_PIN    -1
 #endif
 #ifndef E1_CS_PIN
   #define E1_CS_PIN        P4_29 // BSD2660 default
